d04589aad6116d28fc746266db50c7ee56430c6a
uthai <uthai.u@kbtg.tech>
2019-09-16 12:42:02 +0700

    test

--- boboobua/src/main/java/com/example/boboobua/BoboobuaApplication.java
+++ boboobua/src/main/java/com/example/boboobua/BoboobuaApplication.java
@@ -2,12 +2,18 @@ package com.example.boboobua;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class BoboobuaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoboobuaApplication.class, args);
 	}
-
+	@GetMapping("/")
+	public String hello() {
+		return "Hello Docker";
+	}
 }
